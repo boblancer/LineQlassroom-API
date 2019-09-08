7fb148648182e17a42cb2b9933c558aabdf2c5db
Bob <janekarnkit2013@hotmail.com>
2019-09-08 23:42:25 +0700

    add webhook

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 import src.MessagingApiRoute
 app = Flask(__name__)
 #app.register_blueprint(src.MessagingApiRoute.app)
@@ -20,10 +20,21 @@ handler = WebhookHandler('ff13ecf3c4dc16c47bee15857376aab4')
 line_bot_api = LineBotApi('7ZhYcRofXp0gR++3VC0aXV+Xtt36XSsCtpqu8Hpwh/L2b70FE0wN5G2SkPW5QqjLMGobF9FXglqyxB3A+YotCPN5'
                           'JZk2nzBYtiu76OxYnv9BBSmaUbLHW5hA2IG3odqJkrEwd2vFu3JC8s7g7VDajgdB04t89/1O/w1cDnyilFU=')
 
-@app.route('/bot')
-def bottest():
-    line_bot_api.push_message("Ue952fb83bb2975a6ec362d1fe8cf8338", TextSendMessage(text='Hello World!'))
-    return jsonify({'botbot': 'hello student'})
+@app.route("/webhook", methods=['POST'])
+def webhook():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
 
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_message(event):
@@ -45,6 +56,8 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
+
+
 
 if __name__ == "__main__":
     app.run()
