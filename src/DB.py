4808a18668f057fad08501a21a1f483aab541a9b
Bob <janekarnkit2013@hotmail.com>
2019-09-10 01:11:41 +0700

    fix incorret wording

--- src/DB.py
+++ src/DB.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 from google.cloud import storage
 import json
-
+'''
 with open('ServiceAccountSecretKey.json') as dataFile:
     data = dataFile.read()
     obj = data[data.find('{') : data.rfind('}')+1]
@@ -11,6 +11,7 @@ with open('ServiceAccountSecretKey.json') as dataFile:
 cred = credentials.Certificate("../ServiceAccountSecretKey.json")
 default_app = firebase_admin.initialize_app(cred)
 firestore_db = firestore.client()
+'''
 
 storage = storage.Client()
 bucket = storage.get_bucket("gs://line-qlassroom-7bed0.appspot.com")
