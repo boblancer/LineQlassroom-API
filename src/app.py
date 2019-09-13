baa96677ae968771af6fa1c6ca9e78754799a4f8
Bob <janekarnkit2013@hotmail.com>
2019-09-13 17:10:05 +0700

    state management

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, abort
+from flask import Flask, request, jsonify, abort, current_app
 import src.MessagingApiRoute
 import firebase_admin
 from firebase_admin import credentials, firestore
@@ -12,10 +12,12 @@ db = firestore.client()
 doc_ref = db.collection(u'metadata').document(u'config')
 storage = storage.Client()
 bucket = storage.get_bucket("line-qlassroom2019.appspot.com")
-state = src.CreateHomeworkModel.State()
 
 app = Flask(__name__)
 app.register_blueprint(src.MessagingApiRoute.app)
+with app.app_context():
+    # within this block, current_app points to app.
+    current_app.state = src.CreateHomeworkModel.State()
 @app.route('/')
 def home():
     return jsonify({'Qlassroom': 'hello student'})
