3d804cf9b00eeb44a3e589d2ae7150805c245daf
Bob <janekarnkit2013@hotmail.com>
2019-09-14 17:01:59 +0700

    quick commit

--- src/clock.py
+++ src/clock.py
@@ -2,7 +2,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', second=1)
+@sched.scheduled_job('interval', minutes=1)
 def timed_job():
     print('cron This job is run every three minutes.')
 
