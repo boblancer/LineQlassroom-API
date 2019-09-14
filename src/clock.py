46f2232891d6c4b9f6aac2cecccda808d067e6cf
Bob <janekarnkit2013@hotmail.com>
2019-09-14 17:16:22 +0700

    local time

--- src/clock.py
+++ src/clock.py
@@ -1,13 +1,10 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-sched = BlockingScheduler()
+sched = BlockingScheduler(timezone="Asia/Bangkok")
 
-@sched.scheduled_job('interval', minutes=1)
-def timed_job():
-    print('cron This job is run every three minutes.')
 
-@sched.scheduled_job('cron', hour=16)
+@sched.scheduled_job('cron', hour=17, minute=17)
 def scheduled_job():
-    print('This job is run every weekday at 4pm.')
+    print('This job is run every day at 5pm 17.')
 
 sched.start()
